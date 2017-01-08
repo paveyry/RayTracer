1d9eed2f59efd73435c63cc6d87e54d2f078f8fe
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2017-01-08 17:14:40 +0100

    shapes: sphere: add intersection function and normal vector getter

--- src/shapes/Sphere.cc
+++ src/shapes/Sphere.cc
@@ -1,3 +1,5 @@
+#include <limits>
+
 #include "Sphere.hh"
 
 namespace shapes
@@ -12,12 +14,33 @@ Sphere::Sphere(cv::Vec3i center, double radius, cv::Vec3b color, double alpha, R
 
 double Sphere::intersect(const cv::Vec3i& raySource, const cv::Vec3i rayDir) const
 {
-    return 0.;
+    std::vector<double> intersections;
+    cv::Vec3i dist = raySource - center_;
+
+    // Solve second degree equation
+    double a = rayDir.dot(rayDir);
+    double b = 2 * rayDir.dot(dist);
+    double c = dist.dot(dist) - radius_ * radius_;
+    double delta = b * b - 4 * a * c;
+    if (delta < 0)
+        return -1;
+    intersections.push_back(-b + sqrt(delta) / (2 * a));
+    intersections.push_back(-b + sqrt(delta) / (2 * a));
+
+    // Find closest intersection
+    double closestIntersection = std::numeric_limits<double>::max();
+    for (size_t i = 0; i < intersections.size(); ++i)
+        if (intersections[i] > 0 && intersections[i] < closestIntersection)
+            closestIntersection = intersections[i];
+
+    // Return the closest intersection if there was a valid intersection
+    return (closestIntersection != std::numeric_limits<double>::max()) ? closestIntersection : -1;
+
 }
 
 cv::Vec3i Sphere::getNormalVect(const cv::Vec3i pt) const
 {
-    return cv::Vec3i{0, 0, 0};
+    return cv::normalize(pt - center_);
 }
 
 }

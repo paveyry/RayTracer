59af4182d4878bec2450f027d4d05e3ebc2b68fc
Olivier Laugier <ol.laugier@gmail.com>
2017-01-08 18:16:02 +0100

    Camera: add transformView matrix

--- src/Camera.cc
+++ src/Camera.cc
@@ -13,4 +13,16 @@ Camera::Camera(cv::Vec3i pos, cv::Vec3i lookAt, cv::Vec3i up, double fov, int wi
     U_ = cv::normalize(viewDirection_.cross(up_));
     V_ = cv::normalize(up_.cross(viewDirection_));
     angle_ = tan((M_PI/2)*fov_/180);
+
+    cv::Vec3i eye;
+    eye.val[0] = U_.dot(pos_) * -1;
+    eye.val[1] = V_.dot(pos_) * -1;
+    eye.val[2] = viewDirection_.dot(pos_) * -1;
+    
+    float matrix_values[16] = {U_.val[0], U_.val[1], U_.val[2], eye.val[0],
+                               V_.val[0], V_.val[1], V_.val[2], eye.val[2],
+                               viewDirection_.val[0], viewDirection_.val[1], viewDirection_.val[2], eye.val[2],
+                               0, 0, 0, 1};
+    transformView_ = cv::Mat(4, 4, CV_64F, matrix_values);
+
 }

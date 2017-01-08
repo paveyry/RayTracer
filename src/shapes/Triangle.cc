87f2e3401a6d0b52e159e0d2d4b6f7d6fa04d42a
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2017-01-08 17:37:25 +0100

    shapes: triangle: add intersection and normal functions

--- src/shapes/Triangle.cc
+++ src/shapes/Triangle.cc
@@ -13,11 +13,31 @@ Triangle::Triangle(cv::Vec3i p1, cv::Vec3i p2, cv::Vec3i p3, cv::Vec3b color, do
 
 double Triangle::intersect(const cv::Vec3i& raySource, const cv::Vec3i rayDir) const
 {
-    return 0.;
+    auto e1 = p2_ - p1_;
+    auto e2 = p3_ - p1_;
+    auto h = rayDir.cross(e2);
+    auto a = e1.dot(h);
+    if (a > -0.00001 && a < 0.00001)
+        return -1;
+
+    auto s = raySource - p1_;
+    double f = 1 / a;
+    double u = f * s.dot(h);
+    if (u < 0. || u > 1.)
+        return -1;
+
+    auto q = s.cross(e1);
+    double v = f * rayDir.dot(q);
+    if (v < 0. || u + v > 1.)
+        return -1;
+    double t = f * e2.dot(q);
+    if (t > 0.00001)
+        return t * sqrt(rayDir.dot(rayDir));
+    return -1;
 }
 
-cv::Vec3i Triangle::getNormalVect(const cv::Vec3i pt) const
+cv::Vec3i Triangle::getNormalVect(const cv::Vec3i) const
 {
-    return cv::Vec3i{0, 0, 0};
+    return cv::normalize((p2_ - p1_).cross(p3_ - p1_));
 }
 }

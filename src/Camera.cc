e33cf5d759e0d0d2442e4eb0d5ad65e85e0afbcf
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2017-01-08 17:48:38 +0100

    camera: clean constructor for better performance

--- src/Camera.cc
+++ src/Camera.cc
@@ -1,16 +1,16 @@
 #include "Camera.hh"
 
 Camera::Camera(cv::Vec3i pos, cv::Vec3i lookAt, cv::Vec3i up, double fov, int width, int height)
-    : pos_(pos)
-    , lookAt_(lookAt)
-    , up_(up)
-    , fov_(fov)
-    , width_(width)
-    , height_(height)
+    : pos_{pos}
+    , lookAt_{lookAt}
+    , up_{up}
+    , fov_{fov}
+    , width_{width}
+    , height_{height}
+    , viewDirection_{lookAt - pos}
+    , aspectRatio_{static_cast<double>(width) / static_cast<double>(height)}
 {
-    viewDirection_ = lookAt_ - pos_;
     U_ = cv::normalize(viewDirection_.cross(up_));
     V_ = cv::normalize(up_.cross(viewDirection_));
-    aspectRatio_ = (double)width / (double)height;
     angle_ = tan((M_PI/2)*fov_/180);
 }

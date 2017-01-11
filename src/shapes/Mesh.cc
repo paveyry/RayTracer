fbe4e793cd07b21487581bde982428a254414764
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2017-01-11 20:41:01 +0100

    shapes: mesh: implement transformations for meshes

--- src/shapes/Mesh.cc
+++ src/shapes/Mesh.cc
@@ -1,5 +1,57 @@
-//
-// Created by VEYRY Pierre-Alexandre on 11/01/2017.
-//
-
 #include "Mesh.hh"
+#include "tools.hh"
+
+namespace shapes
+{
+
+Mesh::Mesh(std::vector<Triangle>&& triangles, const cv::Vec3d& center, const cv::Vec3d& color,
+           double alpha, ReflectionType reflectionType)
+    : Shape(color, alpha, reflectionType)
+    , triangles_{triangles}
+    , center_{center}
+{}
+
+std::pair<const Triangle*, double> Mesh::intersect(const cv::Vec3d& raySource, const cv::Vec3d& rayDir) const
+{
+    int closestTriangleIdx = -1;
+    double closestDist = std::numeric_limits<double>::max();
+    for (int i = 0; i < triangles_.size(); ++i)
+    {
+        double dist = triangles_[i].intersect(raySource, rayDir);
+        if (dist < closestDist)
+        {
+            closestDist = dist;
+            closestTriangleIdx = i;
+        }
+    }
+    if (closestTriangleIdx >= 0)
+        return std::pair<const Triangle*, double>(&triangles_[closestTriangleIdx], closestDist);
+    return std::pair<const Triangle*, double>(nullptr, -1);
+}
+
+void Mesh::translate(const cv::Vec3d& vec)
+{
+    center_ += vec;
+    for (auto& triangle : triangles_)
+        triangle.translate(vec);
+}
+
+void Mesh::scale(double factor)
+{
+    for (auto& t : triangles_)
+    {
+        t.p1_ = center_ + (t.p1_ - center_) * factor;
+        t.p2_ = center_ + (t.p2_ - center_) * factor;
+        t.p3_ = center_ + (t.p3_ - center_) * factor;
+    }
+}
+
+void Mesh::rotate(double angleX, double angleY, double angleZ, const cv::Vec3d& origin)
+{
+    center_ = rotateVec(center_, angleX, angleY, angleZ, origin);
+    for (auto& t : triangles_)
+        t.rotate(angleX, angleY, angleZ, origin);
+}
+
+
+}
--- src/shapes/Mesh.hh
+++ src/shapes/Mesh.hh
@@ -1,8 +1,30 @@
 #pragma once
 
+#include "Shape.hh"
+#include "Triangle.hh"
+
+namespace shapes
+{
 
 class Mesh
 {
+public:
+    Mesh(std::vector<Triangle>&& triangles, const cv::Vec3d& center, const cv::Vec3d& color, double alpha,
+         ReflectionType reflectionType);
 
+    //Mesh(std::move(vector));
+
+    std::pair<const Triangle*, double> intersect(const cv::Vec3d& raySource, const cv::Vec3d& rayDir) const;
+
+    void translate(const cv::Vec3d& vec);
+
+    void scale(double factor);
+
+    void rotate(double angleX, double angleY, double angleZ, const cv::Vec3d& origin);
+
+public:
+    std::vector<Triangle> triangles_;
+    cv::Vec3d center_;
 };
 
+}
